032b468c570d6938fa6c62c88021cd6ea65150c2
dungvm <ctvdungvumanh@zamba.vn>
2022-03-09 17:18:14 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import {createRouter, createWebHashHistory} from 'vue-router';
 import auth from '../middleware/auth';
 import {store} from "../stores";
 
@@ -183,7 +183,7 @@ const routes = [
     },
 ]
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: routes
 })
 
