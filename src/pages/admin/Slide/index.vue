3e51d07c3e0c10b2d16ec3914c3c085d5724d1a6
dungvm <ctvdungvumanh@zamba.vn>
2022-05-07 09:36:42 +0700

    update

--- src/pages/admin/Slide/index.vue
+++ src/pages/admin/Slide/index.vue
@@ -1,7 +1,17 @@
 <template>
     <div>
         <div class="admin-slide">
-            <div class="title">Danh sách Slide</div>
+            <div class="header-slide">
+                <div class="title">Danh sách slide</div>
+                <div class="btn-add">
+                    <a-button type="primary" shape="round" @click="showModal">
+                    <template #icon>
+                    <plus-outlined />
+                    </template>
+                    Thêm mới
+                </a-button>
+                </div>
+            </div>
             <div class="slide-table">
                 <a-table :columns="columns" :data-source="listSlide.data" bordered :pagination="false">
                     <template #image="{ record }">
@@ -25,6 +35,31 @@
                 </a-table>
             </div>
         </div>
+        <a-modal v-model:visible="visible" title="Thêm mới Slide" @ok="handleOk">
+        <template #footer>
+            <a-button key="back" @click="handleCancel">Hủy bỏ</a-button>
+            <a-button key="submit" type="primary" :loading="loading" @click="handleOk">Đồng ý</a-button>
+        </template>
+        <div class="input-upload">
+            <a-upload
+                name="avatar"
+                list-type="picture-card"
+                class="avatar-uploader"
+                :show-upload-list="false"
+                :before-upload="beforeUpload"
+                @change="handleChange"
+                @preview="handlePreview"
+            >
+                <img v-if="imageUrl" :src="imageUrl" alt="avatar" />
+                <div v-else>
+                <a-icon :type="loading ? 'loading' : 'plus'" />
+                <div class="ant-upload-text">
+                    Upload
+                </div>
+                </div>
+            </a-upload>
+        </div>
+        </a-modal>
     </div>
 </template>
 <script>
@@ -56,18 +91,19 @@ const columns = [
         slots: {customRender: 'delete'},
     }
 ]
-import { DeleteOutlined, FormOutlined, ExclamationCircleOutlined } from '@ant-design/icons-vue';
+import { DeleteOutlined, FormOutlined, ExclamationCircleOutlined, PlusOutlined } from '@ant-design/icons-vue';
 import Pagination from '../../../components/admin/common/Pagination.vue';
 import api from '../../../api/admin';
 import { createVNode } from 'vue';
 import { Modal } from 'ant-design-vue';
 export default {
-    components:{Pagination, DeleteOutlined, FormOutlined, ExclamationCircleOutlined},
+    components:{Pagination, DeleteOutlined, FormOutlined, ExclamationCircleOutlined, PlusOutlined},
     data() {
         return {
             status: true,
             columns,
             listSlide: [],
+            visible: false,
         }
     },
     created() {
@@ -107,8 +143,17 @@ export default {
                     });
                 },
             });
+        },
+        
+        showModal() {
+            this.visible = true;
+        },
+        handleCancel() {
+            this.visible = false;
+        },
+        handleOk() {
+            console.log('abc');
         }
-
     },
     watch: {
         status: {
@@ -123,9 +168,13 @@ export default {
 <style lang="scss" scoped>
 .admin-slide {
     padding: 30px;
-    .title {
-        font-size: 20px;
-        margin: 0px 0px 30px 0px;
+    .header-slide {
+        display: flex;
+        justify-content: space-between;
+        .title {
+            font-size: 20px;
+            margin: 0px 0px 30px 0px;
+        }
     }
     .anticon-form {
         color: green;
